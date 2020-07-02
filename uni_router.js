04fbbe9bf6d3a61ca51e81d83c1d81b4d7b1fa8a
yinchengnuo <yinnuo96@163.com>
2020-07-02 16:33:23 +0800

    route 增加 type 属性

--- uni_router.js
+++ uni_router.js
@@ -1,27 +1,35 @@
 import Vue from 'vue'
 
-const _$UNI_ACTIVED_PAGE_ROUTES = []
-let _$UNI_ROUTER_PUSH_POP_FUN = () => {}
-const modulesFiles = require.context('@/pages', true, /\.vue$/)
-
-export const route = { // 当前路由对象所在的 path 等信息<===!!!不一定准确!!!===>
+export const route = { // 当前路由对象所在的 path 等信息。默认为首页
 	fullPath: '/pages/index/index',
 	path: '/index',
+	type: 'push',
 	query: {}
 }
 
+let onchange = () => {} // 路由变化监听函数
+const _$UNI_ACTIVED_PAGE_ROUTES = [] // 页面数据缓存
+let _$UNI_ROUTER_PUSH_POP_FUN = () => {} // pushPop resolve 函数
+const _c = obj => JSON.parse(JSON.stringify(obj)) // 简易克隆方法
+const modulesFiles = require.context('@/pages', true, /\.vue$/) // pages 文件夹下所有的 .vue 文件
+
 Vue.mixin({
 	onShow() {
-		const pages = getCurrentPages().map(e => `/${e.route}`).reverse()
-		if (pages[0]) {
-			const now = _$UNI_ACTIVED_PAGE_ROUTES.find(e => e.fullPath == pages[0])
-			now ? Object.assign(route, now) : _$UNI_ACTIVED_PAGE_ROUTES.push(JSON.parse(JSON.stringify(route)))
-			_$UNI_ACTIVED_PAGE_ROUTES.splice(pages.length, _$UNI_ACTIVED_PAGE_ROUTES.length)
+		const pages = getCurrentPages().map(e => `/${e.route}`).reverse() // 获取页面栈
+		if (pages[0]) { // 当页面栈不为空时执行
+			let old = _c(route)
+			const back = pages[0] != route.fullPath
+			const now = _$UNI_ACTIVED_PAGE_ROUTES.find(e => e.fullPath == pages[0]) // 如果路由没有被缓存就缓存
+			now ? Object.assign(route, now) : _$UNI_ACTIVED_PAGE_ROUTES.push(_c(route)) // 已缓存就用已缓存的更新 route 对象
+			_$UNI_ACTIVED_PAGE_ROUTES.splice(pages.length, _$UNI_ACTIVED_PAGE_ROUTES.length) // 最后清除无效缓存
+			if (back) { // 当当前路由与 route 对象不符时，表示路由发生返回
+				onchange(route, old)
+			}
 		}
 	}
 })
 
-const router = {
+const router = new Proxy({
 	route: route, // 当前路由对象所在的 path 等信息,
 	afterEach: to => {}, // 全局后置守卫
 	beforeEach: (to, next) => next(), // 全局前置守卫
@@ -43,9 +51,9 @@ const router = {
 		})
 		return queryString.length === 1 ? '' : queryString.replace(/&$/, '')
 	},
-	_beforeEach(path, fullPath, query) { // 处理全局前置守卫
+	_beforeEach(path, fullPath, query, type) { // 处理全局前置守卫
 		return new Promise(resolve => {
-			this.beforeEach({ path, fullPath, query }, resolve)
+			this.beforeEach({ path, fullPath, query, type }, resolve)
 		})
 	},
 	_next(next) { // 处理全局前置守卫 next 函数传经来的方法
@@ -60,12 +68,12 @@ const router = {
 			}
 		})
 	},
-	_routeTo(UNIAPI, path, query, notBeforeEach, notAfterEach) {
+	_routeTo(UNIAPI, type, path, query, notBeforeEach, notAfterEach) {
 		return new Promise((resolve, reject) => {
 			this._getFullPath(path).then((fullPath) => { // 检查路由是否存在于 pages 中
 				const routeTo = url => { // 执行路由
-					const temp = JSON.parse(JSON.stringify(route)) // 将 route 缓存起来
-					Object.assign(route, { path, fullPath, query }) // 在路由开始执行前就将 query 放入 route, 防止少数情况出项的 onLoad 执行时，query 还没有合并
+					const temp = _c(route) // 将 route 缓存起来
+					Object.assign(route, { path, fullPath, query, type }) // 在路由开始执行前就将 query 放入 route, 防止少数情况出项的 onLoad 执行时，query 还没有合并
 					UNIAPI({ url }).then(([err]) => {
 						if (err) { // 路由未在 pages.json 中注册
 							route = temp // 如果路由跳转失败，就将 route 恢复
@@ -73,6 +81,7 @@ const router = {
 							return
 						} else { // 跳转成功, 将路由信息赋值给 route
 							resolve(route) // 将更新后的路由对象 resolve 出去
+							onchange({ path, fullPath, query, type }, temp)
 							!notAfterEach && this.afterEach(route) // 如果没有禁止全局后置守卫拦截时, 执行全局后置守卫拦截
 						}
 					})
@@ -80,7 +89,7 @@ const router = {
 				if (notBeforeEach) { // notBeforeEach 当不需要被全局前置守卫拦截时
 					routeTo(`${fullPath}${this._formatData(query)}`)
 				} else {
-					this._beforeEach(path, fullPath, query).then((next) => { // 执行全局前置守卫,并将参数传入
+					this._beforeEach(path, fullPath, query, type).then((next) => { // 执行全局前置守卫,并将参数传入
 						this._next(next).then(() => { // 在全局前置守卫 next 没传参
 							routeTo(`${fullPath}${this._formatData(query)}`)
 						}).catch(e => reject(e)) // 在全局前置守卫 next 中取消或重定向路由
@@ -97,25 +106,32 @@ const router = {
 	},
 	// path 路由名 //  query 路由传参 // isBeforeEach 是否要被全局前置守卫拦截 // isAfterEach 是否要被全局后置守卫拦截
 	push(path, query = {}, notBeforeEach, notAfterEach) {
-		return this._routeTo(uni.navigateTo, path, query, notBeforeEach, notAfterEach)
+		return this._routeTo(uni.navigateTo, 'push', path, query, notBeforeEach, notAfterEach)
 	},
 	pushPop(path, query = {}, notBeforeEach, notAfterEach) {
 		return new Promise(resolve => {
 			_$UNI_ROUTER_PUSH_POP_FUN(null)
 			_$UNI_ROUTER_PUSH_POP_FUN = resolve
-			this._routeTo(uni.navigateTo, path, query, notBeforeEach, notAfterEach)
+			this._routeTo(uni.navigateTo, 'pushPop', path, query, notBeforeEach, notAfterEach)
 		})
 	},
 	replace(path, query = {}, notBeforeEach, notAfterEach) {
-		return this._routeTo(uni.redirectTo, path, query, notBeforeEach, notAfterEach)
+		return this._routeTo(uni.redirectTo, 'replace', path, query, notBeforeEach, notAfterEach)
 	},
 	switchTab(path, query = {}, notBeforeEach, notAfterEach) {
-		return this._routeTo(uni.switchTab, path, query, notBeforeEach, notAfterEach)
+		return this._routeTo(uni.switchTab, 'switchTab', path, query, notBeforeEach, notAfterEach)
 	},
 	reLaunch(path, query = {}, notBeforeEach, notAfterEach) {
-		return this._routeTo(uni.reLaunch, path, query, notBeforeEach, notAfterEach)
+		return this._routeTo(uni.reLaunch, 'reLaunch', path, query, notBeforeEach, notAfterEach)
 	}
-}
+}, {
+	set(target, key, value) {
+		if (key == 'onchange') {
+			onchange = value
+		}
+		return Reflect.set(target, key, value)
+	}
+})
 
 Object.setPrototypeOf(route, router) // 让 route 继承 router
 
